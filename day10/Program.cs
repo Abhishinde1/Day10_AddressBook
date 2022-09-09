42ba0e57383cbc5ee6970d858e3437ddf86a336c
AbhiShinde1 <abhishinde572@gmail.com>
2022-09-09 19:48:35 +0530

    Welcome to Address Book

---
+++ day10/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace day10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PersonDetails contact = new PersonDetails();
+            Console.WriteLine("Enter the number of Address Books you want to add:");
+            int noOfAddressBooks = Convert.ToInt32(Console.ReadLine());
+            int noOfBooks = 0;
+
+            while (noOfBooks != noOfAddressBooks)
+            {
+                PersonDetails user = new PersonDetails();
+                string keyPress = "y";
+                Console.WriteLine("Enter the name of address book:");
+                string addressBookName = Convert.ToString(Console.ReadLine());
+
+                Console.WriteLine("Enter your Choice to perform specific operation on address book.");
+                Console.WriteLine();
+                while (keyPress != "n")
+                {
+                    Console.WriteLine("Choose option\n  1:AddContact");
+                    int option = Convert.ToInt32(Console.ReadLine());
+
+                    switch (option)
+                    {
+
+                        case 1:
+                            Console.WriteLine("Enter how many contacts you want to add to address book:");
+                            int number = Convert.ToInt32(Console.ReadLine());
+                            for (int i = 0; i < number; i++)
+
+                                user.AddContact();
+                            break;
+
+                    }
+                }
+            }
+        }
+    }
+}
